23ffae6dec3fc4e2b1f181933b841f6d68568772
NickSettler <moiseevnikita14@gmail.com>
2021-12-18 16:24:09 +0100

    adds some points helper functions

--- src/helpers/mapbuilder/pointsHelpers.ts
+++ src/helpers/mapbuilder/pointsHelpers.ts
@@ -1,6 +1,14 @@
 import Point from "./point";
 import { TRIPLET_ORIENTATION } from "./consts";
 
+/**
+ * Returns point ID based on coordinates. Uses ^P(\d{0,})-(\d{0,})$ regexp to parse coordinates.
+ * @param {number} x - x coordinate
+ * @param {number} y - y coordinate
+ */
+export const pointID = (x: number | string, y: number | string): string =>
+  `P${x}-${y}`;
+
 export const pointsDistance = (point1: Point, point2: Point): number => {
   const x = point1.x - point2.x;
   const y = point1.y - point2.y;
@@ -18,6 +26,19 @@ export const pointOnLine = (
     point.y >= Math.min(lineStart.y, lineEnd.y) &&
     point.y <= Math.max(lineStart.y, lineEnd.y)
   );
+};
+
+const closestPointIndex = (point: Point, points: Array<Point>): number => {
+  let closestDistance = pointsDistance(point, points[0]);
+  let closestIndex = 0;
+  for (let i = 1; i < points.length; i++) {
+    const distance = pointsDistance(point, points[i]);
+    if (distance < closestDistance) {
+      closestDistance = distance;
+      closestIndex = i;
+    }
+  }
+  return closestIndex;
 };
 
 export const tripletOrientation = (
